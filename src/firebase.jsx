72cf93667ac387d7c05e7123715bb7aaa6e92ba7
Jabriel Hans Talula <145314061+jirbthagoras@users.noreply.github.com>
2024-11-23 08:25:04 +0700

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 
-    apiKey: "AIzaSyCXzcpUubhO0IsF9EkbYcv5yUCYTQilWfI",
+    apiKey: "",
   
     authDomain: "react-firebase-41f3e.firebaseapp.com",
   
